f336514cf6f6a0f6a188cd98352aa06058e1bb96
sommisom <somm.clif@gmail.com>
2026-07-20 20:38:02 +0800

    revised-temperature

--- sensor.ino
+++ sensor.ino
@@ -1,4 +1,4 @@
 //Pre-assembled temperature module initialized (no external resistor needed)
-Serial.println ("Reading: 25 C");
-Serial.println("Reading: 77 F");
+Serial.println ("Reading: 0 C");
+Serial.println("Reading: 32 F");
 
