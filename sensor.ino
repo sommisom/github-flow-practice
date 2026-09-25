802b5da6c4d68116384165aae547785736738847
sommisom <somm.clif@gmail.com>
2026-09-25 21:10:53 +0800

    I add a line

--- sensor.ino
+++ sensor.ino
@@ -1,4 +1,5 @@
 //Pre-assembled temperature module initialized (no external resistor needed)
-Serial.println ("Reading: 0 C");
+Serial.println("Reading: 0 C");
 Serial.println("Reading: 32 F");
+Serial.println("haii po");
 
